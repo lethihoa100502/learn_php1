f4d8333a24d228fe25ba3682c06dc6392d751d9b
lethihoa100502 <lethihoa10052002@gmail.com>
2023-07-14 09:43:52 +0700

    lession 13

--- lession 13/Bài 1 IF.php
+++ lession 13/Bài 1 IF.php
@@ -1,20 +1,17 @@
 <?php
+
 interface Resizable {
-    public function resize($percentage);
+    public function resize($ratio);
 }
 
+// Triển khai interface "Resizable" trên lớp "Rectangle"
 class Rectangle implements Resizable {
-    private $width;
-    private $height;
+    protected $width;
+    protected $height;
 
     public function __construct($width, $height) {
         $this->width = $width;
         $this->height = $height;
-    }
-
-    public function resize($percentage) {
-        $this->width *= $percentage / 100;
-        $this->height *= $percentage / 100;
     }
 
     public function getWidth() {
@@ -24,12 +21,15 @@ class Rectangle implements Resizable {
     public function getHeight() {
         return $this->height;
     }
+
+    public function resize($ratio) {
+        $this->width *= $ratio;
+        $this->height *= $ratio;
+    }
 }
 
-// Sử dụng:
-$rectangle = new Rectangle(10, 20);
-echo "Width: " . $rectangle->getWidth() . ", Height: " . $rectangle->getHeight() . "\n";
+$rectangle = new Rectangle(10, 5);
+echo "Kích thước ban đầu: " . $rectangle->getWidth() . "x" . $rectangle->getHeight() . "<br>";
 
-$rectangle->resize(50);
-echo "Width after resizing: " . $rectangle->getWidth() . ", Height after resizing: " . $rectangle->getHeight() . "\n";
-?>
+$rectangle->resize(3);
+echo "Kích thước sau khi thay đổi: " . $rectangle->getWidth() . "x" . $rectangle->getHeight() . "<br>";
